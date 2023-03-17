6bed09f5b28b7e494fe89bb8aec2a6fc5436a7e1
Gianluca Aguzzi <gianluca.aguzzi@studio.unibo.it>
2023-03-17 11:14:04 +0100

    prepare lab 3

--- 04-lists-and-streams/src/test/scala/u02/BTreesTest.scala
+++ 04-lists-and-streams/src/test/scala/u02/BTreesTest.scala
@@ -4,19 +4,18 @@ import org.junit.*
 import org.junit.Assert.*
 import BTrees.Tree.*
 
-class BinaryTreesTest {
+class BTreesTest:
+
   val tree = Branch(Branch(Leaf(1), Leaf(2)), Leaf(1))
 
-  @Test def testCount(): Unit = {
+  @Test def testCount() =
     assertEquals(2, count(tree, 1))
     assertEquals(1, count(tree, 2))
     assertEquals(0, count(tree, 3))
-  }
-  @Test def testSize(): Unit =
+
+  @Test def testSize() =
     assertEquals(3, size(tree))
 
-  @Test def testFind(): Unit = {
+  @Test def testFind() =
     assertTrue(find(tree, 2))
     assertFalse(find(tree, 4))
-  }
-}
